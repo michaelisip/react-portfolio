af064c50f68c8ed4f9c62d287a1b4eab1530d890
Michael John Isip <mj.isip123@gmail.com>
2020-02-11 01:17:25 +0800

    emoji

--- src/components/about/about.component.js
+++ src/components/about/about.component.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Emoji } from 'emoji-mart'
 
 const About = ({ about, email }) => (
     <section className="about text-left my-5" id="about">
         <h1 className="header my-5">
-            Hey there! <br />
+            Hey there! <Emoji emoji="wave" set="apple" skin="3" size={40} /> <br />
             My name is <u>MJ.</u> { about.about }
         </h1>
         <p className="info"> 
