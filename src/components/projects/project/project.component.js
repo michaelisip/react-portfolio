44ff8c4b548586514b8e6f3dfbf4936d5db6dc4d
Michael John Isip <mj.isip123@gmail.com>
2020-02-23 17:18:48 +0800

    project link

--- src/components/projects/project/project.component.js
+++ src/components/projects/project/project.component.js
@@ -6,12 +6,9 @@ const Project= ({ index, project, endpoint }) => (
     <div className="d-flex flex-column flex-md-row align-items-center my-5">
         <div className={((index % 2) === 0 ? 'order-md-1' : 'order-md-2') + " mb-3 mt-5"} style={{ maxWidth: '800px' }}>
             <h1 className="project-title"> 
-              { project.name }&nbsp;
-              { project.url &&
-                <a href={project.url} target="blank" rel="noopener noreferrer">
-                  <FontAwesomeIcon icon={faEye} size="xs" />
-                </a>
-              }
+              <a href={project.url ? project.url : null} target="blank" rel="noopener noreferrer" className="text-dark">
+                { project.name }
+              </a>
             </h1>
             <p className="project-role"> { project.role } </p>
             <p className="project-description"> 
